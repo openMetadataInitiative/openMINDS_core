ff8e7c10d2f9323228f60be5f85b908332b1dfef
Stefan Koehnen <s.koehnen@fz-juelich.de>
2019-08-21 13:24:47 +0200

    Add print for file contents

--- v3.0/validator/validate_schemes.py
+++ v3.0/validator/validate_schemes.py
@@ -3,6 +3,8 @@ from pathlib import Path
 def main():
     for filename in Path('..').glob('**/*.schema.json'):
         print(filename)
+        f = open(filename,'r')
+        print(f.read())
 
 
 if __name__ == "__main__":
