b06bc11f0f8fadd1a4df486474b91308391dd8a7
Stefan Koehnen <s.koehnen@fz-juelich.de>
2019-08-21 15:36:22 +0200

    Fixed bug in exception handling

--- v3.0/validator/validate_schemes.py
+++ v3.0/validator/validate_schemes.py
@@ -10,10 +10,9 @@ def main():
         with open(filename,'r') as f:
             try:
                 Draft7Validator.check_schema(json.loads(f.read()))
+                print(str(filename) + ": PASSED")
             except:
                 print(str(filename) + " failed validation")
-
-            print(str(filename) + ": PASSED")
 
 
 if __name__ == "__main__":
