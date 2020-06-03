00cbf8c5e08cc110af4cb6f205728b4fb53e9da1
Stefan Köhnen <s.koehnen@fz-juelich.de>
2020-06-03 12:07:59 +0200

    Update return types of schema validation

--- v3.0/validator/validate_schemes.py
+++ v3.0/validator/validate_schemes.py
@@ -8,12 +8,18 @@ import json
 def main():
     for filename in Path('..').glob('**/*.schema.json'):
         with open(filename,'r') as f:
+            errors = False
             try:
                 Draft7Validator.check_schema(json.loads(f.read()))
                 print(str(filename) + ": PASSED")
             except:
                 print(str(filename) + " failed validation")
+                errors = True
 
+    if errors:
+        sys.exit(-1)
+    else:
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
