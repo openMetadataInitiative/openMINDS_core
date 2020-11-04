37d9be7c16fd3c8a0052546439ff2436d7e665fd
Oliver Schmid <oliver.schmid@epfl.ch>
2020-11-04 15:41:16 +0100

    fix issues and add generator

--- validate_schemas.py
+++ validate_schemas.py
@@ -1,26 +1,19 @@
 from jsonschema import Draft7Validator
 from pathlib import Path
 
-import sys
 import json
 
 
 def main():
-    errors = False
-    for filename in Path('..').glob('**/*.schema.json'):
-        with open(filename,'r') as f:
+    for filename in Path('target').glob('**/*.schema.json'):
+        with open(filename, 'r') as f:
             try:
-                json.loads(f.read())
+                Draft7Validator.check_schema(json.loads(f.read()))
                 print(str(filename) + ": PASSED")
             except Exception as e:
                 print(str(filename) + " failed validation")
                 print(e)
-                errors = True
 
-    if errors:
-        sys.exit(-1)
-    else:
-        sys.exit(0)
 
 if __name__ == "__main__":
     main()
